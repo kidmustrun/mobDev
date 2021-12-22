ca8f4012afbfae1b436fd9d27c5bb1d7282cad4c
Irina <parakerta@mail.ru>
2021-12-22 14:20:45 +0300

    5hw

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,32 +1,56 @@
 package com.example.myapplication
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.ActivityMainBinding
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private val verticalLinearLayoutManager: LinearLayoutManager =
-        LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+    private var btn: Button?=null ;
+    private var statusText: TextView ?=null;
+    private var stringBuilder:StringBuilder?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.recyclerView.layoutManager = verticalLinearLayoutManager
-        binding.recyclerView.adapter =
-            PersonAdapter(PersonList.createCollectionItems(), ::showCardMessage, ::showLikeMessage)
+        setContentView(R.layout.activity_main)
+        btn= findViewById<Button>(R.id.btn_async);
+        statusText=findViewById<TextView>(R.id.statusText)
+        statusText?.movementMethod=ScrollingMovementMethod()
+        stringBuilder = StringBuilder("Вывод сообщений на экран\n")
+        statusText?.text = "${stringBuilder.toString()}"
+        btn?.setOnClickListener(View.OnClickListener {
+            var  task:MyAsyncTask = MyAsyncTask()
+            task.execute(10)
+        })
     }
-
-    private fun showLikeMessage(person: Person) {
-        val toast = Toast.makeText(applicationContext, "Нажат лайк:  " + person.name, Toast.LENGTH_SHORT)
-        toast.show()
-    }
-
-    private fun showCardMessage(person: Person) {
-        val toast = Toast.makeText(applicationContext, "Нажата карточка:  " + person.name, Toast.LENGTH_SHORT)
-        toast.show()
+    inner class MyAsyncTask : AsyncTask<Int, Int, Int>(){
+        override fun doInBackground(vararg params: Int?): Int {
+            val count:Int?=params[0]
+            var index=0
+            while (index< count!!){
+                publishProgress(index+1)
+                Thread.sleep(2000)
+                index++
+            }
+            return count;
+        }
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+            if(values[0]!=null) {
+                stringBuilder?.append("Сообщение с номером ${values[0]}\n")
+                statusText?.text = stringBuilder.toString()
+            }
+        }
+        override fun onPreExecute() {
+            super.onPreExecute()
+            statusText?.text = "${stringBuilder.toString()}"
+        }
+        override fun onPostExecute(result: Int?) {
+            super.onPostExecute(result)
+            statusText?.text = "${stringBuilder.toString()}"
+        }
     }
 }
